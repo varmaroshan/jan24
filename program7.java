b0520aa25f270a7c5502febc177e9c8fa6509eba
varmaroshan <roshanvarmaneelam@gmail.com>
2025-01-24 14:51:34 +0530

    first commit

---
+++ program7.java
@@ -0,0 +1,28 @@
+
+public class program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * 
+ *                1
+ *               2 2
+ *             3  3  3
+ *           4  4  4  4
+ *         5  5  5  5  5  
+ */
+		for(int r = 1,sp=10;r<=5;r++) {
+			for(int k =1;k<sp;k++) {
+				System.out.print(" ");
+			}
+		sp = sp-1;
+		 for(int c=1;c<=r;c++) {
+				System.out.print(r+" ");
+			}
+			System.out.println();
+			}
+		}
+	}
+
+
+
